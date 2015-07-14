e88ef91f561ca5075b8033dbcbd07e86b6dfa3ec
markalolo <mark_stedman@wycliffeassociates.org>
2015-07-14 10:20:47 -0400

    more changes to mark.js

--- scripts/mark.js
+++ scripts/mark.js
@@ -15,41 +15,20 @@
 //   WycliffeAssociates
 
 module.exports = function (robot) {
-
-    // Updates the brain's knowledge.
-//    function updateBrain(version) {
-//        robot.brain.set('version', version);
-//    }
-
-    // Returns all boards.
-//    function getBoards() {
-//        return robot.brain.get('boards') || {};
-//    }
-
-    // Hear the command
-//    robot.respond(/bible me [\\"\\'](.+)[\\"\\']$/i, function (msg) {
-//        updateBrain(msg);
-//        msg.send("Ok, from now on Bible Me will return: " + robot.brain.get('version');
-//    });
-
-robot.respond(/bible version ULB/i, function(res) {
-  var version;
-  version = "ULB"
-    robot.brain.set('version', version);
-    res.reply('Version set to ULB');
+  robot.respond(/bible version *(.*)/i, function(res) {
+  var version
+  version = res.match[1]
+  if (version == undefined) {
+      version = robot.brain.get('version')
+      res.reply('Using ' + version);
+  } else if ((version == 'UDB') || (version == 'ULB')) {
+      robot.brain.set('version', version);
+      res.reply('Version set to ' + version);
+  } else {
+      res.reply('Sorry, ' + version + 'is not available')
+  }
+    
 });
 
-robot.respond(/bible version UDB/i, function(res) {
-  var version;
-  version = "UDB"
-  robot.brain.set('version', version);
-  res.reply('Version set to ' + version);
-});
-
-robot.respond(/bible version/i, function(res) {
-  var version;
-  version = robot.brain.get('version')
-    res.reply('2nd func' + version);
-});
 
 };
